af4c3988cbfe297d43cf777978ba36045e7dec4c
KUGAKU <githubgemini.0521@gmail.com>
2023-08-05 10:21:29 +0900

    TypeWriterEffect watcher対応

--- components/TypewriterEffect.vue
+++ components/TypewriterEffect.vue
@@ -3,25 +3,31 @@
 </template>
 
 <script setup lang="ts">
+import { ref, watchEffect, onUnmounted } from "vue";
+
 const props = defineProps({
   text: { type: String, required: true },
-  speed: {
-    type: Number,
-    default: 50,
-  },
+  speed: { type: Number, default: 50 },
 });
 
-const displayedText = ref("");
-let index = 0;
+let displayedText = ref("");
+let intervalId: number | undefined;
 
-onMounted(() => {
-  const intervalId = setInterval(() => {
+watchEffect(() => {
+  displayedText.value = "";
+  let index = 0;
+  intervalId && clearInterval(intervalId);
+  intervalId = window.setInterval(() => {
     displayedText.value += props.text.charAt(index);
     index++;
-    if (index > props.text.length) {
-      clearInterval(intervalId);
+    if (index >= props.text.length) {
+      intervalId && clearInterval(intervalId);
     }
   }, props.speed);
+});
+
+onUnmounted(() => {
+  intervalId && clearInterval(intervalId);
 });
 </script>
 
